2869acded51f7bfa36ad8940d19a3c7a62d4d15d
Sankarnath Suresh <sankarnathsuresh@gmail.com>
2024-02-13 12:06:16 -0500

    Initial Commit

---
+++ Sankarnath_Flights/Sankarnath_FlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sankarnath_FlightsApp.swift
+//  Sankarnath_Flights
+//
+//  Created by Sankarnath Suresh on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Sankarnath_FlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
